5e4747b92ee45a25fdfa5639b2b961a02d25c45f
rodmirsantana <rodmirandasantana@gmail.com>
2024-03-04 19:39:50 -0300

    finish base project using App router

---
+++ next.config.js
@@ -0,0 +1,15 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "rodrigo-nextjs-demo-users-image.s3.amazonaws.com",
+        port: "",
+        pathname: "/**",
+      },
+    ],
+  },
+};
+
+module.exports = nextConfig;
